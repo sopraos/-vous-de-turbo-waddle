afe6017913a76a5ed3a549ceeb0bb2653189783e
Sopraos <sopraos@outlook.fr>
2018-02-19 23:06:22 +0100

    Durée de l’exécution

--- config/fish/.confg/fish/functions/theme/fish_prompt.fish
+++ config/fish/.confg/fish/functions/theme/fish_prompt.fish
@@ -14,6 +14,33 @@ function parse_git_branch -d "Parse current Git branch name"
     or string split '\n' (command git show-ref --head -s --abbrev)[1]
 end
 
+function format_time -d "Format milliseconds to a human readable format"
+  set -l milliseconds $argv[1]
+  set -l seconds (math "$milliseconds / 1000 % 60")
+  set -l minutes (math "$milliseconds / 60000 % 60")
+  set -l hours (math "$milliseconds / 3600000 % 24")
+  set -l days (math "$milliseconds / 86400000")
+  set -l time
+  set -l threshold $argv[2]
+
+  if test $days -gt 0
+    set time (command printf "$time%sd " $days)
+  end
+
+  if test $hours -gt 0
+    set time (command printf "$time%sh " $hours)
+  end
+
+  if test $minutes -gt 0
+    set time (command printf "$time%sm " $minutes)
+  end
+
+  if test $seconds -gt $threshold
+    set time (command printf "$time%ss " $seconds)
+  end
+
+  echo -e $time
+end
 
 
 # Whether or not is a fresh session
@@ -135,6 +162,12 @@ function pre_prompt --on-event fish_prompt
     set pre_prompt $pre_prompt $user_and_host
   end
 
+  # Prompt command execution duration
+  if test -n "$CMD_DURATION"
+    set command_duration (format_time $CMD_DURATION $command_max_exec_time)
+  end
+
+  set pre_prompt $pre_prompt "$color_yellow$command_duration$color_normal"
 
   echo -e -s $pre_prompt
 end
